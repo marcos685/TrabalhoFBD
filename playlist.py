bbf1219a6392ec892325b6fb844d12c91c2c2bbd
marcos685 <marcos_gomes@outlook.com>
2018-12-07 13:43:05 -0300

    V1.01

--- playlist.py
+++ playlist.py
@@ -2,7 +2,7 @@ import pyodbc
 cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER=localhost;DATABASE=bdspotper;UID=SA;PWD=Admin123')
 cursor = cnxn.cursor()
 
-cursor.execute("SELECT cod, nome FROM album")
+cursor.execute("SELECT cod, nome FROM albuns")
 albuns_query = cursor.fetchall()
 
 for row in albuns_query:
@@ -17,17 +17,17 @@ while cod_album != -1:
 	albuns_selecionados.append(cod_album)
 	cod_album = input()
 
-
+faixas_selecionadas = []
 for album in albuns_selecionados:
 	print album
-	cursor.execute("SELECT f.numero, f.descricao FROM faixas f, album a WHERE f.cod_album = a.cod")
+	cursor.execute("SELECT f.numero, f.descricao FROM faixas f, albuns a WHERE f.cod_album = ?", album)
 	for row in cursor.fetchall():
 		print row
 
 	print ("entre com os numeros das faixas que deseja inserir na playlist, "
 		"uma de cada vez, seguido de um enter. Quando terminar entre com -1 \n")
 	numero = input()
-	faixas_selecionadas = []
+	
 	while numero != -1:
 		faixas_selecionadas.append((numero, album)) 
 		numero = input()
@@ -35,18 +35,16 @@ for album in albuns_selecionados:
 
 playlist_nome = input("De um nome para sua playlist:\n")
 
-cursor.execute("SELECT max(cod) FROM playlist")
-playlist_cod = 0
-if not(cursor.fetchval() is None):
-	cursor.fetchval() + 1
+cursor.execute("SELECT max(cod) FROM playlists")
+playlist_cod = cursor.fetchval() + 1
 
 #for faixa in faixas_selecionadas:
 #	cursor.execute("SELECT tempo_execucao FROM faixas WHERE numero = ? AND cod_album = ?", faixa[0], faixa[1])
 #	playlist_tempo += cursor.fetchval()
-playlist_tempo = 0
+playlist_tempo = '6000'
 
-cursor.execute("INSERT INTO playlist (cod, nome) VALUES (?, ?)", 
-				playlist_cod, playlist_nome)
+cursor.execute("INSERT INTO playlists (cod, nome, tempo_execucao) VALUES (?, ?, ?)", 
+				playlist_cod, playlist_nome, playlist_tempo)
 
 for faixa in faixas_selecionadas:
 	cursor.execute("INSERT INTO faixa_playlist (faixa_numero, faixa_album, playlist) VAlUES (?, ?, ?)", \
